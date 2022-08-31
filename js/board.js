0e67e99a4e53bbf1c6a87d37e46e52dcf7c6c368
domitre2003 <domitre2003@gmail.com>
2022-08-31 17:05:09 -0400

    test

--- js/board.js
+++ js/board.js
@@ -1,22 +1,23 @@
-function loadScript(url) {    
+function loadScript(url) {
     var head = document.getElementsByTagName('head')[0];
     var script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = url;
     head.appendChild(script);
 }
-
-loadScript('/js/jquery-1.7.min.js');
-loadScript('/js/startGame')
-
-import {startGame2} from './startGame.js'
-
 var board = Chessboard('board2', {
-    draggable: true,    
+    draggable: true,
     dropOffBoard: 'trash',
     sparePieces: true
 })
 
+loadScript('/js/jquery-1.7.min.js');
+loadScript('/js/startGame')
 
-$('#startBtn').on('click', startGame2.startGame(board))
+import { startGame } from './startGame.js'
+
+
+
+
+$('#startBtn').on('click', startGame(board))
 $('#clearBtn').on('click', board.clear)
