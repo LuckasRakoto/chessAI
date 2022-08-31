a21d640021c68a526b7d810ead8e53ff18c12e9f
Luckas Rakoto <luckasrakoto@gmail.com>
2022-08-31 16:33:43 -0400

    on comprend rien

--- js/board.js
+++ js/board.js
@@ -1,9 +1,22 @@
+function loadScript(url) {    
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+    head.appendChild(script);
+}
+
+loadScript('/js/jquery-1.7.min.js');
+loadScript('/js/startGame')
+
+import {startGame2} from './startGame.js'
+
 var board = Chessboard('board2', {
-    draggable: true,
+    draggable: true,    
     dropOffBoard: 'trash',
     sparePieces: true
 })
 
 
-$('#startBtn').on('click', board.start)
+$('#startBtn').on('click', startGame2.startGame(board))
 $('#clearBtn').on('click', board.clear)
---
+++ js/startGame.js
@@ -0,0 +1,9 @@
+import * as chessjs from 'chess'
+
+function startGame(board) {
+    var game = new Chess()
+    board.start
+    console.log('on est là')
+}
+
+export const startGame2 = {startGame}
